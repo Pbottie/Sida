28da4c1b5af50eef948679c6ba272276f130c5f2
Pbottie <pbottie@gmail.com>
2017-03-23 11:36:57 +0100

    Update getBus.php

--- getBus.php
+++ getBus.php
@@ -3,6 +3,8 @@
   error_reporting(E_ALL);
   $key= include 'key.php';
 
-  $content = file_get_contents("http://api.sl.se/api2/realtimedepartures.json?key={$key}&siteid=9140&timewindow=60");
+// $content = file_get_contents("http://api.sl.se/api2/realtimedepartures.json?key={$key}&siteid=9140&timewindow=60");
+$content = file_get_contents("http://api.sl.se/api2/realtimedeparturesV4.json?key={$key}&siteid=9140&timewindow=60;
+");
   echo $content;
   ?>
